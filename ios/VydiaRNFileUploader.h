d998e7dd8bad1a76e663f531a2b30f77fe11a3cd
shovon ghosh <shovon1010@gmail.com>
2022-05-23 21:50:47 +0600

    ios bg upload

---
+++ ios/VydiaRNFileUploader.h
@@ -0,0 +1,10 @@
+#import <Foundation/Foundation.h>
+#import <MobileCoreServices/MobileCoreServices.h>
+#import <React/RCTEventEmitter.h>
+#import <React/RCTBridgeModule.h>
+
+@interface VydiaRNFileUploader : RCTEventEmitter <RCTBridgeModule, NSURLSessionTaskDelegate>
+    +(VydiaRNFileUploader*)sharedInstance;
+    -(void)setBackgroundSessionCompletionHandler:(void (^)(void))handler;
+    @property (atomic) BOOL isObserving;
+@end
